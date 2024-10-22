733092f7a3b3998c2d036cf93840f322f66f90dd
jeff355 <jeff@symptomtrace.com>
2024-10-22 21:12:16 +0800

    Eliminate Cors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import "dotenv/config"
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors( { origin: 'https://localhost:8081' }));
   await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
