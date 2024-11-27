790ea0115951848db422539849bd940453d74a16
HiIamJeff67 <iamjeffhi67@gmail.com>
2024-11-28 00:11:27 +0800

    test: Try Database Automated Delete Expired PurchaseOrders and SupplyOrders using Vercel Cron Job

--- src/cron/cron.controller.ts
+++ src/cron/cron.controller.ts
@@ -1,13 +1,46 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, Res } from '@nestjs/common';
 import { CronService } from './cron.service';
 import { Response } from 'express';
+import { HttpStatusCode } from '../enums';
 
 @Controller('api/cron')
 export class CronController {
   constructor(private readonly cronService: CronService) {}
 
+  // note that all the Cron Jobs are using GET Method
+  /* ================================= Automated Delete operations ================================= */
+  @Get('deleteExpiredPurchaseOrders')
+  async deleteExpiredPurchaseOrders(@Res() response: Response) {
+    try {
+      const res = await this.cronService.deleteExpiredPurchaseOrders();
+
+      response.status(HttpStatusCode.Ok).send({
+        numberOfExpiredPurchaseOrders: res.length, 
+      })
+    } catch (error) {
+      response.status(error.status).send(error.response);
+    }
+  }
+
+  @Get('deleteExpiredSupplyOrders')
+  async deleteExpiredSupplyOrders(@Res() response: Response) {
+    try {
+      const res = await this.cronService.deleteExpiredSupplyOrders();
+
+      response.status(HttpStatusCode.Ok).send({
+        numberOfExpiredSupplyOrders: res.length, 
+      })
+    } catch (error) {
+      response.status(error.status).send(error.response);
+    }
+  }
+  /* ================================= Automated Delete operations ================================= */
+
+
+  /* ================================= Test operations ================================= */
   @Get()
   test(@Res() response: Response) {
     response.status(200).send({context: "Hello Vercel Cron"})
   }
+  /* ================================= Test operations ================================= */
 }
--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -1,6 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DRIZZLE } from '../drizzle/drizzle.module';
+import { DrizzleDB } from '../drizzle/types/drizzle';
+import { PurchaseOrderTable } from '../drizzle/schema/purchaseOrder.schema';
+import { and, eq, lte, or } from 'drizzle-orm';
+import { SupplyOrderTable } from '../drizzle/schema/supplyOrder.schema';
 
 @Injectable()
 export class CronService {
-  
+  constructor(
+    private config: ConfigModule, 
+    @Inject(DRIZZLE) private db: DrizzleDB, 
+  ) {}
+
+  /* ================================= Automated Delete operations ================================= */
+  async deleteExpiredPurchaseOrders() {
+    return await this.db.delete(PurchaseOrderTable)
+      .where(or(
+        eq(PurchaseOrderTable.status, "EXPIRED"), 
+        lte(PurchaseOrderTable.startAfter, new Date()), 
+      )).returning({
+        id: PurchaseOrderTable.id, 
+      });
+  }
+
+  async deleteExpiredSupplyOrders() {
+    return await this.db.delete(SupplyOrderTable)
+      .where(or(
+        eq(SupplyOrderTable.status, "EXPIRED"), 
+        lte(SupplyOrderTable.startAfter, new Date()), 
+      )).returning({
+        id: SupplyOrderTable.id, 
+      });
+  }
+  /* ================================= Automated Delete operations ================================= */
 }
