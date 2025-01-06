5a3c11b4453bcd80939b92dde2920dc1f466e8ca
HiIamJeff67 <iamjeffhi67@gmail.com>
2025-01-07 01:17:42 +0800

    test: Test getCustomerId API Route

--- src/passengerBank/passengerBank.controller.ts
+++ src/passengerBank/passengerBank.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Res } from '@nestjs/common';
 import { PassengerBankService } from './passengerBank.service';
 import { CreatePassengerBankDto } from './dto/create-passengerBank.dto';
 import { UpdatePassengerBankDto } from './dto/update-passengerBank.dto';
+import { Passenger } from '../auth/decorator';
+import { PassengerType } from '../interfaces';
+import { JwtPassengerGuard } from '../auth/guard';
+import { Response } from 'express';
+import { HttpStatusCode } from '../enums';
 
 @Controller('passengerBank')
 export class PassengerBankController {
@@ -10,5 +15,23 @@ export class PassengerBankController {
   @Get('/')
   listCostomers() {
     return this.passengerBankService.listStripeCostomers();
+  }
+
+  @UseGuards(JwtPassengerGuard)
+  @Post('/getCustomerId')
+  async getCustomerId(
+    @Passenger() passenger: PassengerType, 
+    @Res() response: Response, 
+  ) {
+    try {
+      const res = await this.passengerBankService.getPassengerBankByUserId(passenger.id);
+
+      response.status(HttpStatusCode.Ok).send(res);
+    } catch (error) {
+      response.status(error.status).send({
+        case: error.case, 
+        message: error.message, 
+      });
+    }
   }
 }
--- src/passengerBank/passengerBank.service.ts
+++ src/passengerBank/passengerBank.service.ts
@@ -5,15 +5,67 @@ import { STRIPE_CLIENT } from '../stripe/constants';
 import Stripe from 'stripe';
 import { DRIZZLE } from '../drizzle/drizzle.module';
 import { DrizzleDB } from '../drizzle/types/drizzle';
+import { PassengerBankTable } from '../drizzle/schema/passengerBank.schema';
+import { ConfigService } from '@nestjs/config';
+import { ClientCreatePassengerBankException, ClientPassengerBankNotFoundException } from '../exceptions';
+import { eq } from 'drizzle-orm';
 
 @Injectable()
 export class PassengerBankService {
   constructor(
+    private config: ConfigService, 
     @Inject(STRIPE_CLIENT) private stripe: Stripe, 
     @Inject(DRIZZLE) private db: DrizzleDB, 
   ) {}
 
   async listStripeCostomers() {
     return this.stripe.customers.list();
+  }
+
+  async getPassengerBankByUserId(
+    userId: string, 
+  ) {
+    return await this.db.transaction(async (tx) => {
+      const responseOfSelectingPassengerBank = await tx.select({
+        customerId: PassengerBankTable.customerId, 
+      }).from(PassengerBankTable)
+        .where(eq(PassengerBankTable.userId, userId))
+        .limit(1);
+      if (!responseOfSelectingPassengerBank || responseOfSelectingPassengerBank.length === 0) {
+
+        const customer = await this.stripe.customers.create();
+        const ephemeralKey = await this.stripe.ephemeralKeys.create(
+          { customer: customer.id },
+          { apiVersion: '2024-12-18.acacia' }
+        );
+        const paymentIntent = await this.stripe.paymentIntents.create({
+          amount: 0,
+          currency: 'usd',
+          customer: customer.id,
+          automatic_payment_methods: {
+            enabled: true,
+          },
+        });
+
+        const responseOfCreatingPassengerBank = await tx.insert(PassengerBankTable).values({
+          customerId: customer.id, 
+          userId: userId, 
+        }).returning({
+          balance: PassengerBankTable.balance, 
+        });
+        if (!responseOfCreatingPassengerBank || responseOfCreatingPassengerBank.length === 0) {
+          throw ClientCreatePassengerBankException;
+        }
+
+        return {
+          paymentIntent: paymentIntent.client_secret,
+          ephemeralKey: ephemeralKey.secret,
+          customer: customer.id,
+          publishableKey: this.config.get("STRIPE_PK_API_KEY"), 
+        };
+      }
+
+      return responseOfSelectingPassengerBank;
+    });
   }
 }
